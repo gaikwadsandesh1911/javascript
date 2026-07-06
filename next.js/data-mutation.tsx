38126dbc6a21fb90d84590d56f814e0272111f43
sandesh gaikwad <gaikwad.sandesh1911@gmail.com>
2026-07-06 21:42:00 +0530

    Form component

--- next.js/data-mutation.tsx
+++ next.js/data-mutation.tsx
@@ -8,7 +8,7 @@
       - After a mutation, cache invalidation (e.g., revalidatePath or revalidateTag) is often used 
         to ensure users see the latest data.
 
-    The recommended way of data mutation is to use Server Actions.
+    The recommended way of data mutation is by using Server Actions.
 
     Server Actions:
 
@@ -17,7 +17,7 @@
 
         Server Action are defined with "use server" directive
             - at top of async function or
-            - at top of file to mark all exports of that file as Server Actions
+            - at top of file that determines all exports of that file as Server Actions
 
         we use Server Action as the action prop of a <form action={serverActionFunction}>
 
@@ -55,9 +55,12 @@ export async function createProduct(formData: FormData) {
 
   revalidatePath("/products");
   // This tells Next.js to invalidate the cached data for /products, so the next request gets fresh data.
+
+  // OR we can also use redirect("/products")
+  redirect('/products')
 }
 
-// we can also use redirect("/products")
+
 
 
 // app/create-product/page.tsx
@@ -82,7 +85,7 @@ export default function Page() {
         Is a React hook used with Server Action,
         which track the status of a form submission. 
 
-        It is commonly used to show loading states, disable buttons, or display submission progress 
+        It is commonly used to show loading states, disable buttons
         while the form is being submitted.
 
      */    
@@ -129,6 +132,26 @@ export default function Page() {
 
 */
 
+const [state, formAction, isPending] = useActionState(serverAction, initialState);
+
+/* 
+    useActionState takes:
+
+      - a Server Action and 
+      - an initial state. 
+
+    And It returns:
+
+      - the current state, Initially, it is equal to initialState
+      - a wrapped action function (formAction) that is passed to the form action attribute, and 
+      - a boolean (isPending) indicating whether the Server Action is currently executing. 
+      
+
+    Every time the Server Action returns a new state,
+    the state variable is updated and the component re-renders.
+
+  */
+
 // types.ts
 export type FormState = {
   success: string;
@@ -149,6 +172,7 @@ export async function createProduct(prevState: FormState,formData: FormData): Pr
   const price = Number(formData.get("price"));
   const description = formData.get("description") as string;
 
+  // create empty object
   const errors: FormState["errors"] = {};
 
   if (!title.trim()) {
@@ -170,6 +194,7 @@ export async function createProduct(prevState: FormState,formData: FormData): Pr
     };
   }
 
+  // make separate function for db operation and call here, we may use 'use cache' for db operations
   await prisma.product.create({
     data: {
       title,
@@ -265,35 +290,9 @@ export default function Page() {
   );
 }
 
+// -----------------------------------------------------------------------------------
 
-
-// -----------------------------------------------------------
-
-const [state, formAction, isPending] = useActionState(serverAction, initialState);
-
-/* 
-    useActionState takes:
-
-      - a Server Action and 
-      - an initial state. 
-
-    And It returns:
-
-      - the current state, Initially, it is equal to initialState
-      - a wrapped action function (formAction) that is passed to the form, and 
-      - a boolean (isPending) indicating whether the Server Action is currently executing. 
-      
-
-    Every time the Server Action returns a new state,
-    the state variable is updated and the component re-renders.
-
-  */
-
-
-// ------------------------------------------------------------
-// ------------------------------------------------------------
-
-      // Update / Edit 
+      // Update  server action
 
       /* 
             When updating an existing record: 
@@ -481,7 +480,7 @@ const [state, formAction, isPending] = useActionState(serverAction, initialState
 
     /* when we use useActionState(), Server Action expects  (prevState, formData)
     
-      But we also need the product id, we bind it first:
+      But we also need the product id to update , we bind it first:
 
         const updateProductWithId = updateProduct.bind(null, product.id);
 
@@ -494,18 +493,336 @@ const [state, formAction, isPending] = useActionState(serverAction, initialState
 
 // ------------------------------------------------------------
 
+// delete Server Action.
+
+
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+
+export type DeleteState = {
+  success: boolean;
+  message: string;
+};
+
+export async function deleteProduct(
+  id: string,
+  prevState: DeleteState,
+  formData: FormData
+): Promise<DeleteState> {
+  try {
+    await prisma.product.delete({
+      where: { id },
+    });
+
+    revalidatePath("/products");
+
+    return {
+      success: true,
+      message: "Product deleted successfully.",
+    };
+  } catch {
+    return {
+      success: false,
+      message: "Failed to delete product.",
+    };
+  }
+}
+
+
+
+
+"use client";
+
+import { useActionState } from "react";
+import { deleteProduct, type DeleteState } from "./actions";
+
+const initialState: DeleteState = {
+  success: false,
+  message: "",
+};
+
+export default function DeleteButton({
+  id,
+}: {
+  id: string;
+}) {
+  const deleteAction = deleteProduct.bind(null, id);
+
+  const [state, formAction, pending] = useActionState(deleteAction, initialState);
+
+  return (
+    <form action={formAction}>
+      <button disabled={pending}>
+        {pending ? "Deleting..." : "Delete"}
+      </button>
+
+      {state.message && <p>{state.message}</p>}
+    </form>
+  );
+}
+
+// ------------------------------------------------------------
 
 
 
 // useOptimistic()
 
+/* 
+  useOptimistic is react hook use with server action.
+
+  It provides a way to Immediately update the UI, before server confirm the mutaion.
+
+  and if server action failed we can revert the updated state.
+
+*/
+
+const [optimisticState, addOptimistic] = useOptimistic(initialState, updateFn)
+
+/* 
+    It takes,
+
+
+        initialState                               =>    what we want optimistically update, 
+                                                         Basically List of items fetch from db.
+
+        updatFn.                                               
+        (currentState, optimisticValue) => {}      =>    function which Determine how to update state optimistically.
+                                                         It it takes two arguments...
+                                                          
+                                                        - currentState which initially same as initialState,
+                                                          
+                                                        - optimisticValue which help to make new state. basically, 
+                                                            It is id we use to update, 
+                                                            the value you pass to addOptimistic()
+
+    and returns,
+
+
+        optimisticState                           =>    The state that includes optimistic updates.
+                                                        the returned result of function, we use this for rendering. 
+                                                    
+                                                    
+                                        
+        addOptimistic                             =>      A function used to trigger an optimistic update. 
+  
+                                                          The argument you pass becomes the second parameter of updateFn.                                              
+*/
+
+// app/products/actions.ts
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+
+export async function deleteProduct(id: number) {
+  try {
+    
+    await prisma.product.delete({
+      where: {
+        id,
+      },
+    });
+
+    revalidatePath("/products");
+
+    return {
+      success: true,
+    };
+  } catch {
+    return {
+      success: false,
+      message: "Unable to delete product.",
+    };
+  }
+}
+
+
+// app/products/page.tsx
+import { prisma } from "@/lib/prisma";
+import ProductList from "./ProductList";
+
+export default async function Page() {
+  const products = await prisma.product.findMany();
+
+  return <ProductList products={products} />;
+}
+
+
+
+// Client component ProductList
+"use client";
+
+import { useOptimistic } from "react";
+import { deleteProduct } from "./actions";
+
+export default function ProductList({
+  products,
+}: {
+  products: {
+    id: number;
+    title: string;
+  }[];
+}) {
+
+  const [optimisticProducts, removeOptimistic] =
+    useOptimistic(
+      products,
+      (state, deletedId: number) =>
+        state.filter((p) => p.id !== deletedId)
+    );
+
+
+
+  async function handleDelete(id: number) {
+
+    // Optimistically remove immediately
+    removeOptimistic(id);
+
+    const result = await deleteProduct(id);
+
+    if (!result.success) {
+      alert(result.message);
+
+      // React reverts automatically because
+      // the server render still contains
+      // the original products.
+    }
+  }
+
+  return (
+    <>
+      {optimisticProducts.map((product) => (
+        <div key={product.id}>
+          {product.title}
+
+          <button
+            onClick={() => handleDelete(product.id)}
+          >
+            Delete
+          </button>
+        </div>
+      ))}
+    </>
+  );
+}
+
+
+// -----------------------------------------------------------------------------------
+
 
 // Form component
 
+/* Form from next/form,
+
+    - Instead of server action, It handle client side navigation on form submission.
+        eg.   <Form action="/products">
+
+    - It is mainly used for search and filter
+
+    - It automatically prefetches loading.tsx 
+
+    ** Form from next/from expect action prop to be URL/path, 
+      not Server Action. so it can not be used with server action.
+
+*/
+
+/* 
+    app/
+    │
+    ├── products/
+    │   ├── page.tsx
+    │   ├── loading.tsx
+    │   └── SearchForm.tsx
+*/
 
 
 
+/* 
+  If query is undefined, null, or an empty string → fetch all products.
+  Otherwise → return filtered products.
+*/
 
+// app/products/db.ts
+import { prisma } from "@/lib/prisma";
+
+export async function getProducts(query?: string) {
+  if (!query?.trim()) {
+    return prisma.product.findMany({
+      orderBy: {
+        id: "desc",
+      },
+    });
+  }
+
+  return prisma.product.findMany({
+    where: {
+      title: {
+        contains: query,
+        mode: "insensitive",
+      },
+    },
+    orderBy: {
+      id: "desc",
+    },
+  });
+}
+
+
+
+// app/products/SearchForm.tsx
+import Form from "next/form";
+
+export default function SearchForm() {
+  return (
+    <Form action="/products">
+      <input
+        type="text"
+        name="query"
+        placeholder="Search products..."
+      />
+
+      <button type="submit">
+        Search
+      </button>
+    </Form>
+  );
+}
+
+
+// app/products/page.tsx
+
+import SearchForm from "./SearchForm";
+import { getProducts } from "./db";
+
+export default async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    query?: string;
+  }>;
+}) {
+  const { query } = await searchParams;
+
+  const products = await getProducts(query);
+
+  return (
+    <>
+      <h1>Products</h1>
+
+      <SearchForm />
+
+      <hr />
+
+      {products.map((product) => (
+        <p key={product.id}>
+          {product.title}
+        </p>
+      ))}
+    </>
+  );
+}
 
 
 
