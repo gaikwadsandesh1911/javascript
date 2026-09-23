7efc3ccc55926133320cdd018958487500adbc41
sandesh gaikwad <gaikwad.sandesh1911@gmail.com>
2026-09-23 15:59:12 +0530

    updated

--- js/data-types.js
+++ js/data-types.js
@@ -48,6 +48,158 @@
 // ---------------------------------------------------------------------------
 
 
+/* Coercion
+
+    conversion of value from one data type to another is called coercion.
+
+    we have implicit and explicit type of coercion.
+
+    1. explicit: we manually do that.
+
+    2. implicit: 
+            
+        Javascript automatically convert value from one data type to another.
+        It takes places when operands of the expressions are different data types.
+
+        1. String coercion:
+
+            It takes place while using " + " operator.
+            
+            when number added to string.
+            number converted to string type first, then perform operation.
+                
+                let x = 3
+                let y = "3"
+                x + y = "33"
+
+        2.  Number coercion:
+
+            It takes places while using " - " operator.
+        
+            string converted to number first, then perform operaton.
+
+                let x = 3
+                let y = "3"
+                x - y = 0
 
 
+        3. Equality coercion:
+
+            It takes places when using " ==" operator.
+            
+            The ‘ == ‘ operator compares values and not types.
+
+            
+            The ‘==’ operator, converts both the operands to the same type and then compares them.
+
+
+
+        4. Boolean coercion:
+
+            It takes places when using,
+            logical operators, ternary operator, if statement, loop checks.
+
+            To understand Boolean coercion we need to uderstand
+            Truthy and Falsy values.
+
+            Truthy values are those which will be converted into true.
+            False values are those which will be converted into false.
+
+            All values expect false, 0, "", null, undefined, NaN are truthy values.
     
+*/
+
+// if statement
+
+    let a = 0;
+
+    if(a){ 
+        console.log(a);
+    }
+    // The code inside if will not run, since a has falsy value.
+
+// ----------------------------------------------------------------------------------
+    
+
+/*  Logical operators:
+
+    Logical operators in javascript, do not return true or false. 
+    They always return one of the operands.
+
+    OR ( | | ) operator - 
+    
+        If the first value is truthy, then the first value is returned. 
+        Otherwise, always the second value gets returned.
+
+
+    AND ( && ) operator - 
+    
+        If both the values are truthy, always the second value is returned. 
+        If the first value is falsy then the first value is returned or 
+        if the second value is falsy then the second value is returned
+
+*/
+
+    let p = 220
+    let q = "hello"
+    let r = undefined
+
+    p || q    // 220 since first value is truth.
+
+    p || r     // 220 since first value is truty
+
+    p && q     // "hello" since both values are truthy.
+
+    p && r   // undefined, since second value is falsy.
+
+    if( p && q ){
+        console.log('hi...')   // this block runs. because ( p and q return "hello") which is truthyy  value
+    }
+
+// -----------------------------------------------------------
+
+
+/* Passed by value and Passed by reference
+
+     primitive data types are passed by value and 
+
+     non-primitive data types are passed by reference.
+    
+*/
+
+let v1 = 20;
+let v2 = v1;
+
+console.log(v2)
+
+v1 = 30;
+console.log(v2)     // 20
+
+/* 
+    when primitive data types are passed to another variable.
+    new memory address is created for that variable and pass the value.
+
+    so change the value of variable one does not affect the other variable.
+
+*/
+
+let obj1 = {
+    name: "sandesh"
+}
+
+let obj2 = obj1;
+
+console.log(obj2);
+
+obj1.name = "sandesh gaikwad"
+
+console.log(obj2);  // { name: "sandesh gaikwad"}
+
+/* 
+    In non-primitive reference of an object is passed,
+    so both are pointing to same memory address.
+
+    now, changes in one object will affect the other.
+*/
+
+// ----------------------------------------------------------------
